e0e492598c68cd4446a71e0b6d01ba6b29fa36c3
TomasReyes21 <tomasreyes2107gmail.com>
2023-07-29 18:21:41 -0300

    Formularios

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ app.use(express.static('public'))
 app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
